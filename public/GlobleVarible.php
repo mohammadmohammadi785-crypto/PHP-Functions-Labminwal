60845436e4643fd3a38a8e7e199fd659422d736b
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-02-15 10:42:13 +0430

    Changes added

--- public/GlobleVarible.php
+++ public/GlobleVarible.php
@@ -25,8 +25,9 @@
             <form enctype="multipart/form-data" class="flex flex-col gap-3 w-full" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
                 <input name="password" class="h-12 rounded-sm focus:outline-0 p-5 w-full border" type="password">
                 <input type="file" accept="image/*" name="file" class="border file:cursor-pointer border-white w-fit p-3 rounded-sm">
-                <button class="pt-3 pb-3 pl-0 pr-0 text-white border-white border rounded-md " type="submit">Send</button>
+                <button download class="pt-3 pb-3 pl-0 pr-0 text-white border-white border rounded-md " type="submit">Send</button>
                 <?php
+                echo time();
                 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $password = $_POST['password'];
                     $Image = $_FILES['file'];
---
+++ public/test.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * Data => peice of information that is not processed , دیتا یک قطعه از معلومات که ما روی آن پروسیس انجام نداده باشیم
+ * Information => peice of in processed and organized data that can be meningful , قطعه از معلومات که پروسیس و تحلیل شده باشه و یک معنا داشته باشه
+ * DataBase => collection of structured data that helps us to store, manage and retrieve data easly, مجموعه از معلومات ساختار داده شده که کومک میکنه به ما که دیتاره ذخیره کنیم مدیریت رویش داشته باشیم و بتوانیم که ازش استفاده کنیم بصورت آسان
+ * dbms => database management system, سیستم مدیریتی دیتابیس
+ * dbms are set of tools or software that let us to create, store, manage and retrieve data from a database, مجموعه از ابزار و یا برنامه هایی است که برما اجازه میدیه که یک دیتابیس ره بسازیم معلومات را در داخل دیتابیس ذخیره کنیم روی معلومات مدیریت داشته باشیم و همچنان اطلاعات ره بدست بیاریم
+ * DBMS: MYSQL, POSTRESQL, SQL SERVER, ORACLE, MONGODB, SQLITE, FIREBASE, SANITY
+ * why we need database?
+ * File based system: local , duplication, consestant
+ * components of a database:
+ * Database
+ * dbms
+ * user
+ * Hard ware
+ * Soft ware => Php ;
+ * How to create a database
+ */
