60a704a9d96e670ea09bb3eef976ffe7b76d03b8
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-02-16 10:37:11 +0430

    Changes added

--- public/normalization.php
+++ public/normalization.php
@@ -1,5 +1,11 @@
 <?php
 
 /*
- *
+ * How to design the database
+ * Requirment Ggthering => یعنی بدست آوردن معلومات درباره سیستم => client
+ * library => books, borrow, member
+ * 1. Database Design => Normalization , نورمال سازی یا عادی سازی دیتابیس
+ * Introduction to Database Normalization => یک پروسه یی بسیار مهمی است در دیتابیس دیزاین که کمک میکنه که ما دیتابیس معصر و پایدار و همچنان دقیق داشته باشیم
+ * => و هچمنان این پروسه کمک میکنه که بتوانیم روی دیتای خود که در دیتابیس ذخیره میکنیم هم دیتاره مهفوظ نگهداری کنیم و هم روی دیتا مدیریت داشته باشیم
+ * database Normaliaztion =>
  */
