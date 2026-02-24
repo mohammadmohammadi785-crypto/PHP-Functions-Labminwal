1f1b3900f8ea9607f45712ac25dec67f7d2229b8
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-02-24 12:08:17 +0430

    Chanes added

--- public/index.php
+++ public/index.php
@@ -1,24 +1,32 @@
 <?php
-$text = 'Hello PHP I Love learning PHP';
-$convertedToArray = explode(' ', $text);
-print_r($convertedToArray);
-echo '<br> <hr>';
-$backToString = implode(' ', $convertedToArray);
-echo $backToString . '<hr>';
-$backToArray = str_split($backToString, 1);
-print_r($backToArray);
-echo '<hr>';
-printf('Hello %s And Good Bye', 'Ali');
-echo '<hr>';
-$arr = ['AhmadName' => 'Ahmad', 'AliName' => 'Ali', 'MohammadName' => 'Mohammad', 'NazaninName' => 'Nazanin', 'SarasName' => 'Sara', 'laylasName' => 'Layla'];
-echo $arr['NazaninName'];
-echo '<hr>';
-$arr1 = [];
-array_push($arr1, 'Ali');
-array_push($arr1, 'Mohammad');
-array_push($arr1, 'Nazanin');
-array_push($arr1, 'Hamid', 'Ali', 'Fatema', 'Qasim');
-array_pop($arr1);
-array_shift($arr1);
-array_unshift($arr1, 'Sadaf', 'Ali Reza');
-print_r($arr1);
+$connection = new mysqli('localhost', 'root', '', 'my_db');
+$command = 'SELECT * FROM department';
+$result = $connection->query($command);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Showing the data from my db</title>
+    <link rel="stylesheet" href="../src/output.css">
+</head>
+<body>
+    <div class="w-[90%] mt-3 mb-3 mr-auto ml-auto">
+        <table class="w-full border border-black border-collapse">
+            <tr>
+                <th class="border p-3 bg-gray-800 text-white text-center">ID</th>
+                <th class="border p-3 bg-gray-800 text-white text-center">Name</th>
+                <th class="border p-3 bg-gray-800 text-white text-center">Description</th>
+            </tr>
+            <?php while ($dep = $result->fetch_assoc()) { ?>
+            <tr>
+                <td class="border p-3 bg-gray-800 text-white text-center"><?php echo $dep['id'] ?></td>
+                <td class="border p-3 bg-gray-800 text-white text-center"><?php echo $dep['name'] ?></td>
+                <td class="border p-3 bg-gray-800 text-white text-center"><?php echo $dep['description'] ?></td>
+            </tr>
+            <?php } ?>
+        </table>
+    </div>
+</body>
+</html>
