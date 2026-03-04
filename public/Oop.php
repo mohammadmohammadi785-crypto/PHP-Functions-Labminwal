b9f95884812a2a7b50bfef34c9a8fc1b3d6378e5
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-03-04 16:40:24 +0430

    Changes added

--- public/Oop.php
+++ public/Oop.php
@@ -1,67 +1,129 @@
 <?php
-// Object oriented programming
-// کمک می کند که قابل استفاده مججد بسازیم
-// کد را خوانا تر نموده و قابل تغییر نماییم
-// Class(قالب برای اشیا)
-class User
+// inheritance
+class Animal
 {
-    public $username;
-    private $email;
+    private $name;
+    private $age;
 
-    public function getEmail()
+    public function __construct($nam, $sin)
     {
-        return $this->email;
+        $this->name = $nam;
+        $this->age = $sin;
     }
 
-    public function setEmail($email)
+    public function eat()
     {
-        if (strpos($email, '@') > -1) {
-            $this->email = $email;
-        }
+        return "$this->name can eat somthing";
     }
 
-    /*
-     * access modifier:
-     * 1. public یعنی این که میتوانی هم داخل کلاس میتوانی به امو پراپرتی در دسترس باشی بتوانی گیت کنی و بتوانی سیت کنی
-     * 2. private عبارت از اکسیس مودیفایر یه که فقط در داخل کلاس به پراپرتی ها به میتود ها تو در دسترس استی ولی در بیرون کلاس از امو میتود ها استفاده کده نمیتوانی
-     * 3. protacted عبارت از اکسیس مودیفایر یه که ام تو میتوانی که در خود پهرینت می تانی که استفاده کنی و هم ده چایلد های شی
-     */
-    public function __construct($name, $emailAddress)
+    public function drink()
     {
-        $this->username = $name;
-        $this->email = $emailAddress;
+        return "$this->name can drink";
     }
 
-    public function post()
+    public function setName($name)
     {
-        return "$this->username posted something";
+        $this->name = $name;
     }
 
-    public function addFriend()
+    public function getName()
     {
-        return "$this->email added a Friend";
+        return $this->name;
+    }
+};
+$animal1 = new Animal('Sheep', 5);
+
+// echo $animal1->getName();
+class Human extends Animal
+{
+    private $degreeOfEducation;
+    private $lastName;
+
+    public function __construct($name, $age, $degree, $lastName)
+    {
+        $this->degreeOfEducation = $degree;
+        $this->lastName = $lastName;
+        parent::__construct($name, $age);
+    }
+
+    public function Think()
+    {
+        return parent::getName() . ' can Think';
     }
 }
 
-$userOne = new User('Ali Ahmadi', 'aliahmadi@example.com');
-$userTwo = new User('Ahmad', 'ahmad@example.com');
-echo $userOne->addFriend() . '<br>';
-echo $userTwo->setEmail('ahmadali@gmail.com');
-echo $userTwo->getEmail();
-// --------------------------------------------------------------------------------------------------------------------------------------------------
-// echo $userOne->username . '<br>';
-// echo $userOne->email . '<br>';
-// echo $userOne->post() . '<br>';
-// echo $userOne->addFriend();
-// echo '<hr>';
-// // USER TWO
-// $userTwo->username = 'mohammadahmadi';
-// $userTwo->email = 'mohammadahmadi@example.com';
-// echo $userTwo->username . '<br>';
-// echo $userTwo->email . '<br>';
-// echo $userTwo->post() . '<br>';
-// echo $userTwo->addFriend();
+$person1 = new Human('Ali', 19, 12, 'Mohammadi');
+$person1->setName('Ahmad');
+echo $person1->getName() . '<br>';
+echo $person1->Think();
 
-// echo $userOne->post() . '<br>';
-// $userTwo = new User('Mohammad', 'mohammad@example.com');
-// echo $userTwo->post() . '<br>';
+/*
+ * Object oriented programming
+ * کمک می کند که قابل استفاده مججد بسازیم
+ * کد را خوانا تر نموده و قابل تغییر نماییم
+ * Class(قالب برای اشیا)
+ * class User
+ * {
+ *     public $username;
+ *     private $email;
+ *
+ *     public function getEmail()
+ *     {
+ *         return $this->email;
+ *     }
+ *
+ *     public function setEmail($email)
+ *     {
+ *         if (strpos($email, '@') > -1) {
+ *             $this->email = $email;
+ *         }
+ *     }
+ *
+ *
+ *      * access modifier:
+ *      * Encapsolation
+ *      * 1. public یعنی این که میتوانی هم داخل کلاس میتوانی به امو پراپرتی در دسترس باشی بتوانی گیت کنی و بتوانی سیت کنی
+ *      * 2. private عبارت از اکسیس مودیفایر یه که فقط در داخل کلاس به پراپرتی ها به میتود ها تو در دسترس استی ولی در بیرون کلاس از امو میتود ها استفاده کده نمیتوانی
+ *      * 3. protacted عبارت از اکسیس مودیفایر یه که ام تو میتوانی که در خود پهرینت می تانی که استفاده کنی و هم ده چایلد های شی
+ *      *
+ *
+ *     public function __construct($name, $emailAddress)
+ *     {
+ *         $this->username = $name;
+ *         $this->email = $emailAddress;
+ *     }
+ *
+ *     public function post()
+ *     {
+ *         return "$this->username posted something";
+ *     }
+ *
+ *     public function addFriend()
+ *     {
+ *         return "$this->email added a Friend";
+ *     }
+ * }
+ *
+ * $userOne = new User('Ali Ahmadi', 'aliahmadi@example.com');
+ * $userTwo = new User('Ahmad', 'ahmad@example.com');
+ * echo $userOne->addFriend() . '<br>';
+ * echo $userTwo->setEmail('ahmadali@gmail.com');
+ * echo $userTwo->getEmail();
+ * --------------------------------------------------------------------------------------------------------------------------------------------------
+ * echo $userOne->username . '<br>';
+ * echo $userOne->email . '<br>';
+ * echo $userOne->post() . '<br>';
+ * echo $userOne->addFriend();
+ * echo '<hr>';
+ * // USER TWO
+ * $userTwo->username = 'mohammadahmadi';
+ * $userTwo->email = 'mohammadahmadi@example.com';
+ * echo $userTwo->username . '<br>';
+ * echo $userTwo->email . '<br>';
+ * echo $userTwo->post() . '<br>';
+ * echo $userTwo->addFriend();
+ *
+ * echo $userOne->post() . '<br>';
+ * $userTwo = new User('Mohammad', 'mohammad@example.com');
+ * echo $userTwo->post() . '<br>';
+ */
