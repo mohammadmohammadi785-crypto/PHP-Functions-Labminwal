9f90daed12c70d7a4f75131ac8481c0485da6d77
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2026-03-15 13:14:22 +0430

    Changes added

--- public/index.php
+++ public/index.php
@@ -12,12 +12,14 @@ $result = showDepartments();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Showing the data from my db</title>
     <link rel="stylesheet" href="../src/output.css">
 </head>
+
 <body>
     <div class="w-[90%] mt-3 mb-3 mr-auto ml-auto">
         <table class="w-full border border-black border-collapse">
@@ -33,12 +35,15 @@ $result = showDepartments();
                 <td class="border p-3 bg-gray-800 text-white text-center"><?php echo $dep['id'] ?></td>
                 <td class="border p-3 bg-gray-800 text-white text-center"><?php echo $dep['name'] ?></td>
                 <td class="border p-3 bg-gray-800 text-white text-center"><?php echo $dep['description'] ?></td>
-                <td class="border p-3 bg-gray-800 text-white text-center"><a href="update.php?id=<?php echo $dep['id'] ?>">Update</a></td>
-                <td class="border p-3 bg-gray-800 text-white text-center"><a href="delete.php?id=<?php echo $dep['id'] ?>">Delete</a></td>
+                <td class="border p-3 bg-gray-800 text-white text-center"><a
+                        href="update.php?id=<?php echo $dep['id'] ?>">Update</a></td>
+                <td class="border p-3 bg-gray-800 text-white text-center"><a
+                        href="delete.php?id=<?php echo $dep['id'] ?>">Delete</a></td>
             </tr>
             <?php } ?>
-            
+
         </table>
     </div>
 </body>
+
 </html>
